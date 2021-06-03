d67d52c5a07b26bc4ef0dab3d0285824a5831fb1
sandeep poomangalath <sapooman@coupang.com>
2021-06-03 17:20:07 +0800

    general

--- js/general/index.js
+++ js/general/index.js
@@ -8,3 +8,22 @@ const o = {
 for(let i in o) {
     console.log(o[i]);
 }
+
+const moment = require('moment');
+
+const date = '2021-05-21';
+
+let start = moment(`${date}T00:00:00+09:00`);
+
+let end = moment(start).add(1, 'days');
+
+console.log(start.toDate());
+console.log(end.toDate());
+
+const arr = ['d', 'c', 'b']
+
+console.log(arr.sort());
+
+console.log(moment(date).isValid());
+
+console.log(end.add(-7, 'days').format('YYYY-MM-DD'));
