d5526094f9db74c9a7238e3a0443f99df206d031
sandeep poomangalath <sapooman@coupang.com>
2021-06-18 13:23:17 +0800

    tesmp

--- js/general/index.js
+++ js/general/index.js
@@ -1,29 +1,3 @@
-const o = {
-    A: '1',
-    B: '2',
-    C: '3',
-    D: '4'
-};
+const a = [1, 2, 3];
 
-for(let i in o) {
-    console.log(o[i]);
-}
-
-const moment = require('moment');
-
-const date = '2021-05-21';
-
-let start = moment(`${date}T00:00:00+09:00`);
-
-let end = moment(start).add(1, 'days');
-
-console.log(start.toDate());
-console.log(end.toDate());
-
-const arr = ['d', 'c', 'b']
-
-console.log(arr.sort());
-
-console.log(moment(date).isValid());
-
-console.log(end.add(-7, 'days').format('YYYY-MM-DD'));
+console.log(a.slice(0, 50));
